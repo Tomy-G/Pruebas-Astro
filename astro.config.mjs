9d29afcc124befebb1ad5d209023079adbf9eb06
Tomás <tomasgoizuetadp@gmail.com>
2024-01-07 17:34:02 +0100

    fixed config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "hybrid",
   server: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 3000,
+    host: "0.0.0.0"
   },
-  integrations: [react()]
+  integrations: [react()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
