20c0641931f30d8f0b6da83a8b7c6408d5a0c561
Tomás <tomasgoizuetadp@gmail.com>
2024-01-07 16:33:26 +0100

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,30 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+import astroI18next from "astro-i18next";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000,
+  },
+  integrations: [astroI18next(), react()],
 });
+
+
+
+
+// import { defineConfig } from 'astro/config';
+
+// import react from "@astrojs/react";
+
+// // https://astro.build/config
+// export default defineConfig({
+//   integrations: [react()]
+// });
