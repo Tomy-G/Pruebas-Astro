a3b1a8930c77af20f8b6ca1df3fafdf12ebc8eff
Tomás <tomasgoizuetadp@gmail.com>
2024-01-07 13:12:32 +0100

    host

---
+++ astro.config copy.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000,
+  },
+  integrations: [react()],
+});
